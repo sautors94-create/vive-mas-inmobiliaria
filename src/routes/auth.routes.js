7f23abd6027e7d5aaa86785709b8a4748a3af22e
Saul Torres <sau,tors94@gmail.com>
2026-07-02 22:38:01 -0600

    wip: bug3

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registro, login, logout, perfil, misLeads, refreshToken, verificarCodigo, reenviarCodigo, actualizarNotificaciones, subirKyc } = require('../controllers/auth.controller');
+const { registro, login, logout, perfil, misLeads, refreshToken, verificarCodigo, reenviarCodigo, actualizarNotificaciones, actualizarPerfil, subirKyc } = require('../controllers/auth.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 const { upload } = require('../config/cloudinary');
 
