49581c1a8a5e3dc88bc46210622b4a285f26d7e8
Saul Torres <sau,tors94@gmail.com>
2026-08-20 01:44:19 -0600

    new

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.hostinger.com',
-  port: 587,
+  port: 465,
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
